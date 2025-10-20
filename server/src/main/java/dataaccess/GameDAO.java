113ed3d84f87a59a59ee7a381ead8a95fff7541e
Jonathan Christensen <amazingpheonix@gmail.com>
2025-10-20 15:19:31 -0600

    Implemented memory game dao

--- server/src/main/java/dataaccess/GameDAO.java
+++ server/src/main/java/dataaccess/GameDAO.java
@@ -3,11 +3,42 @@ package dataaccess;
 import model.GameData;
 
 import java.util.Collection;
+import java.util.HashMap;
 
 interface GameDAO {
-    public boolean clear();
-    public boolean createGame(GameData game);
-    public GameData getGame(String gameID);
+    public void clear();
+    public void createGame(GameData game) throws DataAccessException;
+    public GameData getGame(int gameID) throws DataAccessException;
     public Collection<GameData> listGames();
-    public boolean updateGame(GameData game);
+    public void updateGame(GameData game) throws DataAccessException;
+}
+
+class MemoryGameDAO implements GameDAO {
+    private HashMap<Integer, GameData> gameTable = new HashMap<>();
+    public void clear() {
+        gameTable.clear();
+    };
+    public void createGame(GameData game) throws DataAccessException {
+        if(gameTable.get(game.gameID()) != null){
+            throw new DataAccessException("Game already exists");
+        }
+        gameTable.put(game.gameID(), game);
+    };
+    public GameData getGame(int gameID) throws DataAccessException{
+        GameData game = gameTable.get(gameID);
+        if(game == null){
+            throw new DataAccessException("Game not found");
+        }
+        return game;
+
+    };
+    public Collection<GameData> listGames() {
+        return gameTable.values();
+    };
+    public void updateGame(GameData game) throws DataAccessException{
+        if(gameTable.get(game.gameID()) == null){
+            throw new DataAccessException("Game not found");
+        }
+        gameTable.put(game.gameID(), game);
+    };
 }
