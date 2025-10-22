c52eaea6ad9b46b9373a393b858b7086b07e7b9c
Jonathan Christensen <amazingpheonix@gmail.com>
2025-10-22 11:18:09 -0600

    Set up endpoints, clear works now

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -1,15 +1,60 @@
 package server;
 
 import io.javalin.*;
+import io.javalin.http.Context;
+import service.ChessService;
 
 public class Server {
 
     private final Javalin javalin;
+    private ChessService service;
 
     public Server() {
-        javalin = Javalin.create(config -> config.staticFiles.add("web"));
 
-        // Register your endpoints and exception handlers here.
+        service = new ChessService();
+
+        javalin = Javalin.create(config -> config.staticFiles.add("web"))
+            .delete("/db", this::deleteDb)
+            .post("/user", this::register)
+            .post("/session", this::login)
+            .delete("/sessions", this::logout)
+            .get("/game", this::listGames)
+            .post("/game", this::createGame)
+            .put("/game", this::joinGame)
+            .exception(Exception.class, this::exceptionHandler);
+    }
+
+    private void deleteDb(Context context) {
+        service.clear();
+
+
+    }
+
+    private void register(Context context) {
+
+    }
+
+    private void login(Context context) {
+
+    }
+
+    private void logout(Context context) {
+
+    }
+
+    private void listGames(Context context) {
+
+    }
+
+    private void createGame(Context context) {
+
+    }
+
+    private void joinGame(Context context) {
+
+    }
+
+    private void exceptionHandler(Exception e, Context context) {
 
     }
 
