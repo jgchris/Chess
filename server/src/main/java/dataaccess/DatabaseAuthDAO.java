f6a946164f23a4af265b7c020ad941cb71a6a43a
Jonathan Christensen <amazingpheonix@gmail.com>
2025-11-02 01:36:24 -0600

    Did most of first dao

--- server/src/main/java/dataaccess/DatabaseAuthDAO.java
+++ server/src/main/java/dataaccess/DatabaseAuthDAO.java
@@ -19,24 +19,58 @@ public class DatabaseAuthDAO implements AuthDAO{
             try (var createTableStatement = conn.prepareStatement(createAuthTable)) {
                 createTableStatement.executeUpdate();
             }
-
         }
-
     }
     @Override
     public void clear() {
+        try (var conn = DatabaseManager.getConnection()) {
+            try (var createTableStatement = conn.prepareStatement("DELETE FROM auth")) {
+                createTableStatement.executeUpdate();
+            }
+        } catch (SQLException | DataAccessException e) {
+            throw new RuntimeException(e);
+        }
+
     }
 
     @Override
     public AuthData createAuth(String user) {
         String token = UUID.randomUUID().toString();
         AuthData auth = new AuthData(token, user);
+        try (var conn = DatabaseManager.getConnection()) {
+            try (var preparedStatement = conn.prepareStatement("INSERT INTO auth (token, username) VALUES(?, ?)")) {
+                preparedStatement.setString(1, token);
+                preparedStatement.setString(2, user);
 
+                preparedStatement.executeUpdate();
+            }
+        } catch (SQLException | DataAccessException e) {
+            throw new RuntimeException(e);
+        }
         return auth;
     }
 
     @Override
     public AuthData getAuth(String authToken) throws DataAccessException {
+        try (var conn = DatabaseManager.getConnection()) {
+            try (var preparedStatement = conn.prepareStatement("SELECT token, username FROM auth WHERE token=?")) {
+                preparedStatement.setString(1, authToken);
+                try (var rs = preparedStatement.executeQuery()) {
+                    if (rs.next()) {
+                        var id = rs.getInt("id");
+                        var name = rs.getString("name");
+                        var type = rs.getString("type");
+
+                        System.out.printf("id: %d, name: %s, type: %s%n", id, name, type);
+                    } else {
+                        throw new DataAccessException("Not authorized");
+                    }
+                }
+            }
+
+        } catch (SQLException e) {
+            throw new DataAccessException("Error getting auth token", e);
+        }
     }
 
     @Override
