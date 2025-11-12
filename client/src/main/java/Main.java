f6524b8de573a9835b3f2ac17a5e407a9ee40ffa
Jonathan Christensen <amazingpheonix@gmail.com>
2025-11-11 23:49:04 -0700

    Set up program

--- client/src/main/java/Main.java
+++ client/src/main/java/Main.java
@@ -1,8 +1,12 @@
 import chess.*;
+import ui.LoggedOutMenu;
 
 public class Main {
     public static void main(String[] args) {
-        var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
-        System.out.println("♕ 240 Chess Client: " + piece);
+        System.out.println("♕ 240 Chess Client");
+        System.out.println("Type help for options or quit to exit");
+        LoggedOutMenu startMenu = new LoggedOutMenu("[CHESS (LOGGED OUT)]");
+        startMenu.loop();
+        System.out.println("Have a good day!");
     }
 }
