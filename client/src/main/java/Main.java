1e9b875e2659921a1a8b1e174f2acbe51774ee13
Jonathan Christensen <amazingpheonix@gmail.com>
2025-11-14 09:37:12 -0700

    Removed server dependency from client

--- client/src/main/java/Main.java
+++ client/src/main/java/Main.java
@@ -1,19 +1,15 @@
 import chess.*;
 import client.ServerFacade;
-import server.Server;
 import ui.LoggedOutMenu;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("♕ 240 Chess Client");
         System.out.println("Type help for options or quit to exit");
-        int port = 8080;
-        Server server = new Server();
-        port = server.run(port);
+        int port = 0;
         ServerFacade facade = new ServerFacade(port);
         LoggedOutMenu startMenu = new LoggedOutMenu("[CHESS (LOGGED OUT)]", facade);
         startMenu.loop();
         System.out.println("Have a good day!");
-        server.stop();
     }
 }
