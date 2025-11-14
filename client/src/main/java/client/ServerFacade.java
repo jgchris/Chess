59bf9f03b626ce9876a658be301350a840f9e99a
Jonathan Christensen <amazingpheonix@gmail.com>
2025-11-14 09:44:43 -0700

    Fixed various naming and other problems

--- client/src/main/java/client/ServerFacade.java
+++ client/src/main/java/client/ServerFacade.java
@@ -12,86 +12,86 @@ import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.util.Collection;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 public class ServerFacade {
     private final HttpClient httpClient = HttpClient.newHttpClient();
-    private final String prefix_url;
+    private final String prefixUrl;
     public ServerFacade(int port) {
-        prefix_url = String.format("http://localhost:%d", port);
+        prefixUrl = String.format("http://localhost:%d", port);
     }
     public AuthData register(UserData user) throws ServerError {
         var body = new Gson().toJson(user);
-        var url = this.prefix_url + "/user";
+        var url = this.prefixUrl + "/user";
         var response = sendRequest(url, "POST", body, null);
         if (response.statusCode() == 200) {
             return new Gson().fromJson(response.body(), AuthData.class);
         }
 
-        var error_body = new Gson().fromJson(response.body(), HashMap.class);
-        String error_message = error_body.get("message").toString();
-        throw new ServerError(error_message);
+        var errorBody = new Gson().fromJson(response.body(), HashMap.class);
+        String errorMessage = errorBody.get("message").toString();
+        throw new ServerError(errorMessage);
 
     }
     public AuthData login(UserData user) throws ServerError {
         var body = new Gson().toJson(user);
-        var url = this.prefix_url + "/session";
+        var url = this.prefixUrl + "/session";
         var response = sendRequest(url, "POST", body, null);
         if (response.statusCode() == 200) {
             return new Gson().fromJson(response.body(), AuthData.class);
         }
-        var error_body = new Gson().fromJson(response.body(), HashMap.class);
-        String error_message = error_body.get("message").toString();
-        throw new ServerError(error_message);
+        var errorBody = new Gson().fromJson(response.body(), HashMap.class);
+        String errorMessage = errorBody.get("message").toString();
+        throw new ServerError(errorMessage);
 
     }
     public void logout(String token) throws ServerError {
-        var url = this.prefix_url + "/session";
+        var url = this.prefixUrl + "/session";
         var response = sendRequest(url, "DELETE", null, token);
         if(response.statusCode() > 299) {
-            var error_body = new Gson().fromJson(response.body(), HashMap.class);
-            String error_message = error_body.get("message").toString();
-            throw new ServerError(error_message);
+            var errorBody = new Gson().fromJson(response.body(), HashMap.class);
+            String errorMessage = errorBody.get("message").toString();
+            throw new ServerError(errorMessage);
         }
     }
     public Collection<GameData> listGames(String token) throws ServerError {
-        var url = this.prefix_url + "/game";
+        var url = this.prefixUrl + "/game";
         var response = sendRequest(url, "GET", null, token);
         if (response.statusCode() == 200) {
             Type type = new TypeToken<Map<String, Collection<GameData>>>(){}.getType();
             Map<String, Collection<GameData>> info= new Gson().fromJson(response.body(), type);
             return info.get("games");
         }
-        var error_body = new Gson().fromJson(response.body(), HashMap.class);
-        String error_message = error_body.get("message").toString();
-        throw new ServerError(error_message);
+        var errorBody = new Gson().fromJson(response.body(), HashMap.class);
+        String errorMessage = errorBody.get("message").toString();
+        throw new ServerError(errorMessage);
 
     }
     public int createGame(String token, String name) throws ServerError {
-        var url = this.prefix_url + "/game";
+        var url = this.prefixUrl + "/game";
         GameData game = new GameData(null, null, null, name, null);
         String body = new Gson().toJson(game);
         var response = sendRequest(url, "POST", body, token);
         if (response.statusCode() == 200) {
-            var info_body =  new Gson().fromJson(response.body(), HashMap.class);
-            double id = (double) info_body.get("gameID");
+            var infoBody =  new Gson().fromJson(response.body(), HashMap.class);
+            double id = (double) infoBody.get("gameID");
             return (int) id;
         }
-        var error_body = new Gson().fromJson(response.body(), HashMap.class);
-        String error_message = error_body.get("message").toString();
-        throw new ServerError(error_message);
+        var errorBody = new Gson().fromJson(response.body(), HashMap.class);
+        String errorMessage = errorBody.get("message").toString();
+        throw new ServerError(errorMessage);
 
     }
     public void joinGame(String token, String playerColor, int gameId) throws ServerError {
-        var url = this.prefix_url + "/game";
+        var url = this.prefixUrl + "/game";
         JoinRequest request = new JoinRequest(playerColor, gameId);
         var body = new Gson().toJson(request);
         var response = sendRequest(url, "PUT", body, token);
         if (response.statusCode() != 200) {
-            var error_body = new Gson().fromJson(response.body(), HashMap.class);
-            String error_message = error_body.get("message").toString();
-            throw new ServerError(error_message);
+            var errorBody = new Gson().fromJson(response.body(), HashMap.class);
+
+            String message = errorBody.get("message").toString();
+            throw new ServerError(message);
         }
 
     }
